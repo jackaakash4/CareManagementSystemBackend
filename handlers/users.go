c52a054eef60a1d18d90a9130c64798fd6ce0e3e
Jack Aakash <jackaakash4@gmail.com>
2025-09-02 21:07:04 +0545

    Feat: getUsers handler is created

--- handlers/users.go
+++ handlers/users.go
@@ -1,4 +1,4 @@
-package main
+package users
 
 import (
 	"context"
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -38,4 +39,27 @@ func createUser(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, user)
 
+}
+
+func getUsers(c *gin.Context) {
+	collection := db.Collection("users")
+	cursor, err := collection.Find(context.TODO, bson.M{"is_active": true})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to fetch the user's data",
+		})
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	var users []User
+	if err = cursor.All(context.TODO(), &users); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to decode the users",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
 }
