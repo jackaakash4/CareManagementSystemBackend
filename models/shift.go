ef3867775d21f20ca1f47c1004bafecd8b05bdba
Jack Aakash <jackaakash4@gmail.com>
2025-09-02 20:42:53 +0545

    Feat: Shift's model is created

---
+++ models/shift.go
@@ -0,0 +1,30 @@
+package shift
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Shift struct {
+	ID        primitive.ObjectID `bson: "_id, omitempty" json: "id"`
+	ClientID  primitive.ObjectID `bson: "client_id" json: "client_id"`
+	StaffID   primitive.ObjectID `bson: "staff_id" json: "staff_id"`
+	StartTime time.Time          `bson: "start_time" json: "start_time"`
+	EndTime   time.Time          `bson: "end_time" json: "end_time"`
+	Status    string             `bson: "status" json: "status"` //scheduled, in_process, completed, cancelled
+
+	//gps tracking(optional)
+	CheckInLocation  *Location `bson: "checkin_location" json: "checkin_location"`
+	CheckOutLocation *Location `bson: "checkout_location" json: "checkout_location"`
+
+	//ShiftDetails
+	ActualStartTime *time.Time `bson: "actual_start_time" json: "actual_start_time"`
+	ActualEndTime   *time.Time `bson: "actual_end_time" json: "actual_end_time"`
+
+	//Notes and Reports
+	Notes []Note `bson: "notes" json: "notes"`
+
+	CreatedAt time.Time `bson: "created_at" json: "created_at"`
+	UpdatedAt time.Time `bson: "updated_at" json: "updated_at"`
+}
